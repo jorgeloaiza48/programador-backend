b879808edd652e58214096c5b5b1bed2fb0ca06e
jorgeloaiza48 <jelm48@misena.edu.co>
2023-04-04 10:46:38 -0500

    ++--

--- routes/olvidoPassword/olvidoPassword.js
+++ routes/olvidoPassword/olvidoPassword.js
@@ -7,12 +7,12 @@ const nodemailer = require("nodemailer")
 const JWT_SECRTET = "some super secret..."
 const path = require('path')
 //const envJSON = require('../../variablesEntorno.json')
-//let URL = ""
+let URL = ""
 //console.log(envJSON.development.SERVER_URL)
 
 router.post("/", (req, res) => {
 
-    const NODE_ENV = process.env.NODE_ENV || 'development' //si la variable NODE_ENV no está definida entonce la crea con valor "development"   
+    //const NODE_ENV = process.env.NODE_ENV || 'development' //si la variable NODE_ENV no está definida entonce la crea con valor "development"   
 
     let config = {
         method: 'GET',
@@ -31,17 +31,17 @@ router.post("/", (req, res) => {
                 }
                 token = jwt.sign(payload, JWT_SECRTET, { expiresIn: '5m' })
 
-                if (NODE_ENV !== 'production') {  //Está línea pregunta si no se está en un entorno de producción
+                //if (NODE_ENV !== 'production') {  //Está línea pregunta si no se está en un entorno de producción
                 //link = `http://localhost:3000/#/reset-password/${userFilter[0].id}/${token}`
                 require('dotenv').config()   //carga las variables del archivo .env
                 URL = process.env.URL
-                }
-                else {
+                //}
+                //else {
                 //  link = `https://programador-cursos.onrender.com/#/reset-password/${userFilter[0].id}/${token}`
-                URL = process.env.URL //en producción
-                }
+                //URL = process.env.URL //en producción
+                //}
                 link = `${URL}/#/reset-password/${userFilter[0].id}/${token}`
-                
+
                 // create reusable transporter object using the default SMTP transport               
                 const transporter = nodemailer.createTransport({
                     service: "Gmail",
