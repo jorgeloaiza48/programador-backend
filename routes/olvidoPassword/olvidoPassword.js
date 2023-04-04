c555a18319159d6f653fed2ae497ba9c8dd066aa
jorgeloaiza48 <jelm48@misena.edu.co>
2023-04-04 14:51:14 -0500

    +++

--- routes/olvidoPassword/olvidoPassword.js
+++ routes/olvidoPassword/olvidoPassword.js
@@ -9,10 +9,11 @@ const path = require('path')
 //const envJSON = require('../../variablesEntorno.json')
 //let URL = ""
 //console.log(envJSON.development.SERVER_URL)
+let link = ""
 
 router.post("/", (req, res) => {
 
-    //const NODE_ENV = process.env.NODE_ENV || 'development' //si la variable NODE_ENV no está definida entonce la crea con valor "development"   
+    const NODE_ENV = process.env.NODE_ENV || 'development' //si la variable NODE_ENV no está definida entonce la crea con valor "development"   
 
     let config = {
         method: 'GET',
@@ -31,15 +32,15 @@ router.post("/", (req, res) => {
                 }
                 token = jwt.sign(payload, JWT_SECRTET, { expiresIn: '5m' })
 
-                //if (NODE_ENV !== 'production') {  //Está línea pregunta si no se está en un entorno de producción
-                //link = `http://localhost:3000/#/reset-password/${userFilter[0].id}/${token}`
-                //require('dotenv').config()   //carga las variables del archivo .env
+                if (NODE_ENV !== 'production') {  //Está línea pregunta si no se está en un entorno de producción
+                link = `http://localhost:3000/#/reset-password/${userFilter[0].id}/${token}`
+                require('dotenv').config()   //carga las variables del archivo .env
                 //URL = process.env.URL
-                //}
-                //else {
-                  link = `https://programador-cursos.onrender.com/#/reset-password/${userFilter[0].id}/${token}`
+                }
+                else {
+               link = `https://programador-cursos.onrender.com/#/reset-password/${userFilter[0].id}/${token}`
                 //URL = process.env.URL //en producción
-                //}
+                }
                 //link = `${URL}/#/reset-password/${userFilter[0].id}/${token}`
 
                 // create reusable transporter object using the default SMTP transport               
