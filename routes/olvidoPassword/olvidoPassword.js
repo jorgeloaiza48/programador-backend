8f3efa9e6cd000157ec2f09af207bba9bde4a301
jorgeloaiza48 <jelm48@misena.edu.co>
2023-04-04 09:41:49 -0500

    ++--

--- routes/olvidoPassword/olvidoPassword.js
+++ routes/olvidoPassword/olvidoPassword.js
@@ -12,7 +12,7 @@ const path = require('path')
 
 router.post("/", (req, res) => {
 
-    const NODE_ENV = process.env.NODE_ENV || 'development' //si la variable NODE_ENV no está definida entonce la crea con valor "development"   
+    //let NODE_ENV = process.env.NODE_ENV || 'development' //si la variable NODE_ENV no está definida entonce la crea con valor "development"   
 
     let config = {
         method: 'GET',
@@ -30,16 +30,16 @@ router.post("/", (req, res) => {
                     id: userFilter[0].id
                 }
                 token = jwt.sign(payload, JWT_SECRTET, { expiresIn: '5m' })
-                //link = `${URL}/#/reset-password/${userFilter[0].id}/${token}`
-                if (NODE_ENV !== 'production') {  //Está línea pregunta si no se está en un entorno de producción
-                    link = `http://localhost:3000/#/reset-password/${userFilter[0].id}/${token}`
-                    //require('dotenv').config()   //carga las variables del archivo .env
-                    //URL = process.env.URL
-                }
-                else {
-                    link = `https://programador-cursos.onrender.com/#/reset-password/${userFilter[0].id}/${token}`
-                }
 
+                //if (NODE_ENV !== 'production') {  //Está línea pregunta si no se está en un entorno de producción
+                //link = `http://localhost:3000/#/reset-password/${userFilter[0].id}/${token}`
+                require('dotenv').config()   //carga las variables del archivo .env
+                URL = process.env.URL
+                //}
+                //else {
+                //  link = `https://programador-cursos.onrender.com/#/reset-password/${userFilter[0].id}/${token}`
+                //}
+                link = `${URL}/#/reset-password/${userFilter[0].id}/${token}`
                 // create reusable transporter object using the default SMTP transport               
                 const transporter = nodemailer.createTransport({
                     service: "Gmail",
